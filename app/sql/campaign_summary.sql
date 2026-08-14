97df00ecea14d1174be9a18de93587c34f4f555b
drekicks <aferree@gmail.com>
2026-08-14 13:55:32 -0500

    Update campaign_summary.sql

--- app/sql/campaign_summary.sql
+++ app/sql/campaign_summary.sql
@@ -8,6 +8,8 @@ cr.end_date::date,
 count(cr.customer_id) audience_size,
 sum(cr.cmpgn_rvn) campaign_revenue,
 sum(contact_cost + offer_cost) total_campaign_cost,
+round(count(*) filter (where treatment_grp='TEST' and response_flg='Y' and cnvrsn_flg='Y')::numeric/COUNT(*) FILTER (WHERE treatment_grp='TEST'),2) test_cnvrsn_rate,
+round(count(*) filter (where treatment_grp='CONTROL' and response_flg='Y' and cnvrsn_flg='Y')::numeric/COUNT(*) FILTER (WHERE treatment_grp='CONTROL'),2) control_cnvrsn_rate,
 count(*) filter (where response_flg='Y' and cnvrsn_flg='Y') conversions
 from campaign_results cr join customer_segment cs
 	on cr.customer_id = cs.customer_id
@@ -15,8 +17,8 @@ group by 1,2,3,4,5
 ),-- select * from control_aud;,
 full_fnl as 
 (
-select *, round(100*conversions/audience_size,2)/100 conversion_rate, round(campaign_revenue/audience_size,2) revenue_per_customer, 
-round(campaign_revenue/conversions,2) revenue_per_conversion
+select *, round(conversions::numeric/audience_size,2) conversion_rate, round(campaign_revenue/audience_size,2) revenue_per_customer, 
+round(campaign_revenue/conversions,2) revenue_per_conversion,(test_cnvrsn_rate-control_cnvrsn_rate)*100 absolute_lift
 from cmpgn_aud
 ) select * from full_fnl;
 
